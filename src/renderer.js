0bae0a56534bc831470017d8802aa582eeab3398
Jakub Dobroń <jakub.dobron8@gmail.com>
2022-01-29 15:44:59 +0100

    Starting project from scratch, removed all assets, shaders and geometries. Bumped all lib versions, replaced deprecated dat.gui with lil-gui.

---
+++ src/renderer.js
@@ -0,0 +1,106 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import GUI from "lil-gui";
+
+// Global variables
+
+// Textures loaders
+const textureLoader = new THREE.TextureLoader();
+
+// Model loaders
+const gltfLoader = new GLTFLoader();
+
+// Debug
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 2;
+camera.position.z = 5;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = true;
+controls.enableKeys = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.info.autoReset = false;
+
+/**
+ * Animate
+ */
+
+const tick = () => {
+  // Update objects
+
+  // Update Orbital Controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  //Three-Devtools API
+  if (typeof __THREE_DEVTOOLS__ !== "undefined") {
+    __THREE_DEVTOOLS__.dispatchEvent(
+      new CustomEvent("observe", { detail: scene })
+    );
+    __THREE_DEVTOOLS__.dispatchEvent(
+      new CustomEvent("observe", { detail: renderer })
+    );
+  }
+
+  // Call tick again on the next frame
+  requestAnimationFrame(tick);
+};
+
+tick();
